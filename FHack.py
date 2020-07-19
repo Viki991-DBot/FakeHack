c7c2fd3334ef7fe9e6a5f6a2ae2dc8f7c4632063
Viki991-DBot <65564072+Viki991-DBot@users.noreply.github.com>
2020-07-19 21:18:01 +0700

    Add files via upload
    How Do You Think About This Script??Im a beginner!!

---
+++ FHack.py
@@ -0,0 +1,41 @@
+import os
+import time
+#BATAS GAN
+print("CREDIT : ")
+time.sleep(1)
+print("SCRIPT BY KagamiTaiga#3928")
+print("YouTube : LManChi YT")
+print("Tweet : ViksProject")
+time.sleep(1)
+print()
+print("USE CTRL+C TO STOP HACKING(ANDRO)")
+print("USE CTRL+X TO STOP HACKING(PC)")
+time.sleep(1)
+print("[Targets] - can be filled with gmail , wa numbers , ip")
+target = input("Target : ")
+time.sleep(2)
+print("CONNECTING TO " + target)
+time.sleep(1)
+print("Preparinh Tools!!")
+time.sleep(1)
+print("LETS GET STARTED!")
+time.sleep(3)
+i = 2
+while(i < 1000000):
+   j = 2
+   while(j <= (i/j)):
+      if not(i%j): break
+      j = j + 1
+   if (j > i/j) : print (i, "SENT VIRUS TO " + target)
+   i = i + 1
+time.sleep(1)
+print("TARGET : " + target)
+print("STATUS : Hacked!")
+print("Virus Sent Total : 300000")
+time.sleep(2)
+print("THE HACKING SYSTEM IS STOPPED!")
+print("I Will Clear All Message!")
+time.sleep(1)
+print("Bye!")
+time.sleep(1)
+os.system("clear")
